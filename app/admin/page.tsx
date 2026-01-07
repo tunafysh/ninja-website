15451a28ef0a5c4274898890ec711e7cebce94d4
tunafysh <hanansmani@gmail.com>
2026-01-07 12:43:08 +0100

    fixed some things that typescript complained about

---
+++ app/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="p-4">
+      <p>Work in progress.</p>
+    </div>
+  )
+}
---
+++ app/armory/add/page.tsx
@@ -0,0 +1,7 @@
+export default function Add() {
+    return (
+        <div className="p-4">
+            <p>Work in progress.</p>
+        </div>
+    )
+}
